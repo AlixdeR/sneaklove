60ee6b878425e1253ebbf2ebf482004d188b125d
Pascal Besson <pascalbes@gmail.com>
2020-02-06 11:45:01 +0100

    User and Tag models defined

---
+++ models/Tag.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose"); // import mongoose dependencie
+
+const Schema = mongoose.Schema;
+
+const tagSchema = new Schema({
+  tag: [{
+      type: String,
+      enum: ['loose', 'sporty', 'smart', 'urban']
+});
+
+const tagModel = mongoose.model("tag", tagSchema);
+
+module.exports = tagModel;
