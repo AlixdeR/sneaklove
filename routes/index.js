e1e3b2cfc2d8721498e652913563122d3896d2fd
Pascal Besson <pascalbes@gmail.com>
2020-02-06 12:41:32 +0100

    collection route configured

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const tagModel = require("../models/Tag");
+const sneakerModel = require("../models/Sneaker");
+const userModel = require("../models/User");
 
 router.get("/", (req, res) => {
   res.render("index");
+});
+
+router.get("/sneakers/collection", (req, res) => {
+  Promise.all([tagModel.find(), sneakerModel.find()])
+  .then(dbRes => {
+    console.log(dbRes)
+    //res.render("products", {tags:dbRes[0], sneakers:dbRes[1]});
+  })
+  .catch(err => console.log("error while load sneakers", err))
 });
 
 router.get("/sneakers/:cat", (req, res) => {
@@ -10,15 +22,15 @@ router.get("/sneakers/:cat", (req, res) => {
 });
 
 router.get("/one-product/:id", (req, res) => {
-  res.send("baz");
+  res.render("one_product", sneaker);
 });
 
 router.get("/signup", (req, res) => {
-  res.send("sneak");
+  res.render("signup");
 });
 
 router.get("/signin", (req, res) => {
-  res.send("love");
+  res.render("signin");
 });
 
 
